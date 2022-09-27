c1f141ff3cc547f3f25b5647b6eef78888431e7e
ivuruGG <eve1ue.development@gmail.com>
2022-09-27 19:48:45 +0900

    main

--- Go/Basic/main/if・for.go
+++ Go/Basic/main/if・for.go
@@ -22,4 +22,37 @@ func main() {
 			fmt.Println("hogeは負の値です")
 		}
 	}
+
+	// for 初期値; 条件式; 変化式
+	for i := 0; i < 10; i++ {
+		// 処理を記述
+	}
+
+	// 変数宣言での多重代入を用いることで複数の宣言を使ったループが可能
+	for i, j := 0, 1; i > 9 && j > 10; i, j = i+1, j+1 {
+		fmt.Printf("i: %d, j: %d", i, j)
+	}
+
+	for i := 0; i < 10; i++ {
+		// iが偶数の場合
+		if i%2 == 0 {
+			// ここでforの先頭に戻る
+			continue
+		}
+		// 奇数のみ表示される
+		fmt.Printf("i: %d", i)
+	}
+
+	for i := 0; i < 10; i++ {
+		if i == 5 {
+			// for文から出る
+			break
+		}
+	}
+	count := 0
+	for count < 10 {
+		fmt.Printf("count: %d", count)
+		count += 1
+	}
+
 }
